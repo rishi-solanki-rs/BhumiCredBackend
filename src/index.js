40589c7a9227b881512904cbb73c113f4c47df40
rishi-solanki-rs <166364049+rishi-solanki-rs@users.noreply.github.com>
2026-01-24 19:49:28 +0530

    Update CORS origin for production deployment

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ dotenv.config()
 
 const app = express()
 app.use(cors({
-  origin: ["https://bhumicredbackend.onrender.com","http://localhost:5173"],
+  origin: ["https://bhumi-cred-web.vercel.app","http://localhost:5173"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
